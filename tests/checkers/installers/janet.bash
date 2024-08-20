2bdba3387be37801a457c360d455f9ffd5abed2d
Tomasz Hołubowicz <mail@alternateved.com>
2024-08-20 22:28:08 +0200

    Add installer and tests

---
+++ tests/checkers/installers/janet.bash
@@ -0,0 +1,12 @@
+JANET_VERSION="v1.35.2"
+
+apt-get install -y make gcc
+
+cd "$(mktemp -d)" || exit 1
+
+curl -L https://github.com/janet-lang/janet/archive/refs/tags/$JANET_VERSION.tar.gz | tar -xzv
+cd janet-$JANET_VERSION || exit 1
+make && make install
+
+rm -rf "$(pwd)"
+cd - || exit 1
